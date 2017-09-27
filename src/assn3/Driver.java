3d3cb0154f7de0e9036bd9512c0cd672c6308515
Nathaniel Churchill <natechurchill16@gmail.com>
2017-09-27 08:40:47 -0400

    testing on linux

--- src/assn3/Driver.java
+++ src/assn3/Driver.java
@@ -7,7 +7,15 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello AI");
+		Tester t = new Tester();
+		t.printerTest();
 		UserInterface window = new UserInterface();
+		
+		//Test
+		
+		
+		
+		
 		
 	}
 	
---
+++ src/assn3/Tester.java
@@ -0,0 +1,9 @@
+package assn3;
+
+public class Tester {
+	
+	public void printerTest() { 
+		System.out.println("hellow from the tester");
+	}
+
+}
