9c69dd9c80ed03abdd3aa92daf2f781de7d3c925
Nathaniel Churchill <natechurchill16@gmail.com>
2017-09-26 21:10:45 -0400

    adding a blank UI

--- src/assn3/Driver.java
+++ src/assn3/Driver.java
@@ -1,19 +1,17 @@
 package assn3;
 
+import javax.swing.JFrame;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		
 		System.out.println("Hello AI");
 
-		
-		
-		
-		
-		
-		
+		UserInterface window = new UserInterface();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(700,500);
+		window.setVisible(true);
 		
 	}
 
---
+++ src/assn3/UserInterface.java
@@ -0,0 +1,20 @@
+package assn3;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class UserInterface extends JFrame {	
+	private JButton buttonOne;
+	
+	
+	public UserInterface() {
+		super("Connect Four Game");
+		
+		
+	}
+
+	
+	
+	
+
+}
