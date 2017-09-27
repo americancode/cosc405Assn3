cc2a811f366a8a18f4ee74d7c59e7ba8deb22ee1
Nathaniel Churchill <natechurchill16@gmail.com>
2017-09-26 22:24:17 -0400

    adding some circles

--- src/assn3/Driver.java
+++ src/assn3/Driver.java
@@ -7,12 +7,12 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello AI");
-
 		UserInterface window = new UserInterface();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(700,500);
-		window.setVisible(true);
 		
 	}
+	
+	
+	
+
 
 }
--- src/assn3/UserInterface.java
+++ src/assn3/UserInterface.java
@@ -1,20 +1,95 @@
 package assn3;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class UserInterface extends JFrame {	
-	private JButton buttonOne;
-	
-	
+import javax.swing.*;
+
+public class UserInterface extends JFrame {
+
+	private static final int GRID_SIZE = 7;
+	private Circle[][] circlePanels = new Circle[GRID_SIZE][GRID_SIZE];
+	private JPanel gridPanel = new JPanel(new GridLayout(GRID_SIZE, GRID_SIZE));
+
 	public UserInterface() {
-		super("Connect Four Game");
-		
-		
+		initCircles();
+		JFrame f = new JFrame();
+		f.add(gridPanel);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.pack();
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
 	}
 
+	private void initCircles() {
+		for (int i = 0; i < circlePanels.length; i++) {
+			for (int j = 0; j < circlePanels[i].length; j++) {
+				Circle circle = new Circle();
+				circlePanels[i][j] = circle;
+				gridPanel.add(circle);
+			}
+		}
+	}
+
+	class Circle extends JPanel {
+		private boolean draw = false;
+		private Color color = Color.BLUE;
+
+		public Circle() {
+			setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+			addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					if (isDraw()) {
+						setDraw(false);
+					} else {
+						setDraw(true);
+					}
+				}
+			});
+			
+			
+			
+			
+			
+			
+		}
+
+		public boolean isDraw() {
+			return draw;
+		}
+
+		public void setDraw(boolean draw) {
+			this.draw = draw;
+			repaint();
+		}
+
+		public void setColor(Color color) {
+			this.color = color;
+			repaint();
+		}
+
+		@Override
+		public Dimension getPreferredSize() {
+			return new Dimension(75, 75);
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if (draw) {
+				g.setColor(color);
+				g.fillOval(0, 0, getWidth(), getHeight());
+			}
+		}
+
+	}
 	
 	
 	
+	
+	
+	
+
 
 }
