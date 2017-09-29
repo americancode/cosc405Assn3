48e9b1eba0ff40dc07d70f250fceb432267e3ed8
Eric Olechovski <eolechovski97@gmail.com>
2017-09-28 22:17:47 -0400

    " added horizontal WinCheck" " added vertical WinCheck"

--- src/assn3/StateSpace.java
+++ src/assn3/StateSpace.java
@@ -46,22 +46,127 @@ public class StateSpace {
 	}
 	
 	public int winningState(Node currentState) {
-		boolean winnerIsP1 = false;
-		boolean winnerIsP2 = false;
-		Node state = currentState;
-		int[][] gameState = state.getGameState();
+		int playerWin = 0;
+		
+		playerWin += horizontalWinState( currentState );
+		
+		if (playerWin > 0){
+			return playerWin;
+		}
+		
+		playerWin += verticalWinState( currentState );
+		
+		if (playerWin != 0){
+			return playerWin;
+		}
+//		
+//		playerWin += diagonal1WinState( currentState );
+//		
+//		if (playerWin != 0){
+//			return playerWin;
+//			
+//		}
+//		playerWin += diagonal2WinState( currentState );
+//		
+		return 0;
+	}
+
+	
+	/** Check for diagonal win **/
+	
+	private int diagonal2WinState(Node currentState) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/** Check for diagonal win **/
+	
+	private int diagonal1WinState(Node currentState) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/** Check for vertical win **/
+	
+	private int verticalWinState(Node currentState) {
+		
+		int[][] gameState = currentState.getGameState();
+		
+		for(int a = 0; a < gameState.length+1; a++) {
+			
+			int player1Count = 0;
+			int player2Count = 0;
+			
+			for(int b = 0; b < gameState.length; b++) {
+				int player = gameState[b][a];
+				
+				
+				if (player == 2 ){
+					player2Count++;
+					player1Count = 0;
+				}
+				
+				else if (player == 1){
+					player1Count++;
+					player2Count = 0;
+				}
+		
+			}
+			
+			if (player1Count == 4){
+				return 1;
+			}
+			else if (player2Count == 4){
+				return 2;
+			}
+			
+		
+		}
+		return 0;
+
+	}
+	
+
+	/** Check for horizontal win **/
+	
+	private int horizontalWinState(Node currentState) {
+		
+		int[][] gameState = currentState.getGameState();
 		
 		for(int a = 0; a < gameState.length; a++) {
+			
+			int player1Count = 0;
+			int player2Count = 0;
+			
 			for(int b = 0; b < gameState[a].length; b++) {
-				System.out.println(gameState[a][b]);
+				int player = gameState[a][b];
+				
+				
+				if (player == 2 ){
+					player2Count++;
+					player1Count = 0;
+				}
+				
+				else if (player == 1){
+					player1Count++;
+					player2Count = 0;
+				}
 			}
+			
+			if (player1Count == 4){
+				return 1;
+			}
+			else if (player2Count == 4){
+				return 2;
+			}
+		
 		}
+		return 0;
 
-		
-		
-		
-		
-		return 1;
 	}
+	
+	
+	
+	
 	
 }
