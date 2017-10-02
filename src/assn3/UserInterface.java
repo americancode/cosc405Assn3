4d60154f4ab21fe1b8b6649462fb44ba4664ed7c
Dan Richmond <danrichmond@comcast.net>
2017-10-02 09:33:05 -0400

    Added title to JFrame

--- src/assn3/UserInterface.java
+++ src/assn3/UserInterface.java
@@ -14,6 +14,7 @@ public class UserInterface extends JFrame {
 	public UserInterface() {
 		initCircles();
 		JFrame f = new JFrame();
+		f.setTitle("Connect Four");
 		f.add(gridPanel);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
